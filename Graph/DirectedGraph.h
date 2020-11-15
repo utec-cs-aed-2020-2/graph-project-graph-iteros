b260982ca1057f4670d51d30534154f023e9c57e
renato <luis.carbajal@utec.edu.pe>
2020-11-15 01:22:15 -0500

    implemented basic methods

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -11,94 +11,132 @@ private:
 public:
     DirectedGraph();
 
-    bool insertVertex(string id, TV vertex) override;
+    bool insertVertex(string id, TV vertex);
 
-    bool createEdge(string id1, string id2, TE w) override;
-    bool deleteVertex(string id) override;
-    bool deleteEdge(string id) override;
+    bool createEdge(string id1, string id2, TE w);
+    bool deleteVertex(string id);
+    bool deleteEdge(string start, string end);
 
-    TE &operator()(string start, string end)= 0;
-    float density() = 0;
-    bool isDense(float threshold = 0.5) = 0;
-    bool isConnected()= 0;
-    bool isStronglyConnected() throw();
+    TE &operator()(string start, string end);
+    float density();
+    bool isDense(float threshold = 0.5);
+    bool isConnected();
+    bool isStronglyConnected();
     bool empty();
-    void clear()= 0;
+    void clear();
 
-    void displayVertex(string id)= 0;
-    bool findById(string id) = 0;
-    void display() = 0;
+    void displayVertex(string id);
+    bool findById(string id);
+    void display();
 };
 
+template<typename TV, typename TE>
+DirectedGraph<TV, TE>::DirectedGraph() {this->edges = 0;}
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::insertVertex(string id, TV vertex) {
-
+    Vertex<TV, TE>* v = new Vertex<TV, TE>(vertex);
+    this->vertexes[id] = v;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::createEdge(string id1, string id2, TE w) {
-
+    Edge<TV, TE>* e1 = new Edge<TV, TE>(this->vertexes[id1], this->vertexes[id2], w);
+    this->vertexes[id1]->edges.push_back(e1);
+    this->edges++;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::deleteVertex(string id) {
-
+    if (findById(id)) {
+        for (auto i : this->vertexes) {
+            deleteEdge(i.first, id);
+        }
+        this->vertexes.erase(id);
+        return true;
+    }
+    return false;
 }
 
 template<typename TV, typename TE>
-bool DirectedGraph<TV, TE>::deleteEdge(string id) {
-
+bool DirectedGraph<TV, TE>::deleteEdge(string start, string end) {
+    if (findById(start) && findById(end)) {
+        for (auto i = this->vertexes[start]->edges.begin(); i != this->vertexes[start]->edges.end();) {
+            if ((*i)->vertexes[1] == this->vertexes[end]) {
+                this->vertexes[start]->edges.erase(i++);
+                this->edges--;
+            }
+            else i++;
+        }
+        return true;
+    }
+    return false;
 }
 
 template<typename TV, typename TE>
 TE& DirectedGraph<TV, TE>::operator()(string start, string end) {
-
+    for (auto i : this->vertexes[start]->edges) {
+        if (i->vertexes[1] == this->vertexes[end]) return i->weight;
+    }
 }
 
 template<typename TV, typename TE>
 float DirectedGraph<TV, TE>::density() {
-
+    return (1.0*this->edges)/(this->vertexes.size()*(this->vertexes.size()-1));
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::isDense(float threshold) {
-
+    return density() >= threshold;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::isConnected() {
-
+    //TODO
 }
 
 template<typename TV, typename TE>
-bool DirectedGraph<TV, TE>::isStronglyConnected() throw() {
-
+bool DirectedGraph<TV, TE>::isStronglyConnected() {
+    //TODO
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::empty() {
-
+    return this->vertexes.size() != 0;
 }
 
 template<typename TV, typename TE>
 void DirectedGraph<TV, TE>::clear() {
-
+    this->vertexes.clear();
+    this->edges = 0;
 }
 
 template<typename TV, typename TE>
 void DirectedGraph<TV, TE>::displayVertex(string id) {
-
+    cout << this->vertexes[id]->data << "-> ";
+    for (auto const& j : this->vertexes[id]->edges) {
+        if (this->vertexes[id] == j->vertexes[0]) cout << j->vertexes[1]->data << "(" << j->weight << ")" << " ";
+        else cout << j->vertexes[0]->data << "(" << j->weight << ")" << " ";
+    }
+    cout << endl;
 }
 
 template<typename TV, typename TE>
 bool DirectedGraph<TV, TE>::findById(string id) {
-
+    if (this->vertexes.find(id) == this->vertexes.end()) return false;
+    return true;
 }
 
 template<typename TV, typename TE>
 void DirectedGraph<TV, TE>::display() {
-
+    for (auto i : this->vertexes) {
+        cout << i.second->data << "-> ";
+        for (auto j : i.second->edges) {
+            if (i.second == j->vertexes[0]) cout << j->vertexes[1]->data << "(" << j->weight << ")" << " ";
+            else cout << j->vertexes[0]->data << "(" << j->weight << ")" << " ";
+        }
+        cout << endl;
+    }
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Graph/UndirectedGraph.h"
+#include "Graph/DirectedGraph.h"
 
-#include "Tester/tester.h"
-#include "Parser/parser.h"
+//#include "Tester/tester.h"
+//#include "Parser/parser.h"
 
 using namespace std;
 
@@ -10,8 +12,31 @@ int main(int argc, char *argv[]) {
     std::cout << "MENU GRAPH TESTER" << std::endl;
     std::cout << "================================================" << std::endl;
     
-    Tester::executeExamples();
-    Tester::executeParser();
+    //Tester::executeExamples();
+    //Tester::executeParser();
+    Graph<char, int>* g = new DirectedGraph<char, int>();
+    g->insertVertex("1", 'A');
+    g->insertVertex("2", 'B');
+    g->insertVertex("3", 'C');
+    g->insertVertex("4", 'D');
+
+    g->createEdge("1", "2", 10);
+    g->createEdge("1", "2", 12);
+    g->createEdge("1", "3", 5);
+    g->createEdge("3", "4", 15);
+    g->createEdge("2", "4", 20);
+
+    g->display();
+    cout << endl;
+
+    g->displayVertex("3");
+    cout << endl;
+
+    //g->deleteVertex("2");
+    //g->deleteEdge("1", "2");
+    //g->display();
+
+    cout << g->operator()("2", "4") << endl;
     
     return EXIT_SUCCESS;
 }
